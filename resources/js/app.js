f518dea62748e957d08dc9dcd2040c79813ff4dc
chanz-pclimaco <pclimaco@chanzglobal.com>
2024-01-02 14:27:47 +0800

    Install Vuejs, vue-router, dependencies, connect Vuejs to Laravel 10

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+import { createApp } from 'vue'
+
+import app from './components/App.vue'
+
+import router from './router/index.js'
+
+createApp(app)
+    .use(router)
+    .mount('#app')
+    
---
+++ resources/js/components/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Page not Found</h1>
+    <br>
+    <router-link to="/">Back to home page</router-link>
+</template>
