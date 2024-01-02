5a485c84955b81ee9e72cbd3ff190197a8f1ac96
chanz-pclimaco <pclimaco@chanzglobal.com>
2024-01-02 14:48:57 +0800

    Display all invoice

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Invoice;
 
 class InvoiceController extends Controller
 {
-    //
+    public function get_all_invoice(){
+        $invoices = Invoice::with('customer')->orderBy('id', 'DESC')->get();
+        return response()->json([
+            'invoices' => $invoices
+        ],200);
+    }
 }
